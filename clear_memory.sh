cdf6b479122a694a5612b2717050167eef6a104e
Carlos Augusto Moreno <omorenodovale@gmail.com>
2019-11-07 17:42:51 -0500

    Update script clear_memory

--- clear_memory.sh
+++ clear_memory.sh
@@ -7,40 +7,51 @@ PATH="/bin:/usr/bin:/usr/local/bin"
 percent=80
 
 # Total Memory:
-T_RAM="grep -F "MemTotal:" < /proc/meminfo | awk '{print $2}'"
+T_RAM=`grep -F "MemTotal:" < /proc/meminfo | awk '{print $2}'`
 # Free memory:
-L_RAM="grep -F "MemFree:" < /proc/meminfo | awk '{print $2}'"
+L_RAM=`grep -F "MemFree:" < /proc/meminfo | awk '{print $2}'`
 
 # RAM used by the system:
-U_RAM="expr $T_RAM - $L_RAM"
+U_RAM=`expr $T_RAM - $L_RAM`
 
 # Percentage of RAM used by the system:
-P_USED="expr $U_RAM \* 100 / $T_RAM"
+P_USED=`expr $U_RAM \* 100 / $T_RAM`
 
 echo ================================================================
 date
 echo
 echo "Memory Used: $P_USED %";
 
-if [ $P_USED -gt $percent ]
-then
-DATE=`date`
-echo $DATE >> /var/log/memoria.log
-echo "Memory Used: $P_USED %" >> /var/log/memoria.log
+#if [ $P_USED -gt $percent ];
+echo $P_USED
+echo $percent
 
-echo "Memory above $percent %, cache has been cleared.";
-sync
-# Dropping cache:
-echo 3 > /proc/sys/vm/drop_caches
-# If you are not using debian, uncomment the line below
-# sysctl -w vm.drop_caches=3 
-echo
-free -m
-echo
-echo ================================================================
-else
-echo "Could not perform task!";
-echo ================================================================
-unset percent T_RAM L_RAM U_RAM P_USED
-exit $?
+if [ 90 -gt 80 ];
+  then
+    DATE=`date`
+    echo $DATE >> /var/log/memoria.log
+    echo "Memory Used: $P_USED %" >> /var/log/memoria.log
+
+    echo "Memory above $percent %, cache has been cleared.";
+    sync
+    # Dropping cache:
+    echo 3 > /proc/sys/vm/drop_caches
+
+    for i in {`cat /etc/os-release | grep -e "^ID" | awk -F "=" '{print $2}'`};
+    do
+      if [ $i == "debian" ] || [ $i == "ubuntu" ];
+      then
+        sysctl -w vm.drop_caches=3
+      fi
+    done
+
+    echo
+    free -m
+    echo
+    echo ================================================================
+  else
+    echo "Could not perform task!";
+    echo ================================================================
+    unset percent T_RAM L_RAM U_RAM P_USED
+    exit $?
 fi
